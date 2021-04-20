dff4eb845c87ddf031f90bf559a0fc77f41c73f5
adnirajan <75868045+adnirajan@users.noreply.github.com>
2021-04-20 13:34:37 -0400

    Create Fluent-UDF-Thermal_Conductivity.c

---
+++ Fluent-UDF-Thermal_Conductivity.c
@@ -0,0 +1,29 @@
+/*********************************************************************
+ *  *    UDF for Calculating Thermal Conductivity wth Constant Prandtl#  * *
+ *  UDF 
+ *  UDF 
+ *  Models: 
+ *   	
+ *  Notes: 
+		
+ *  		Problems: 
+ *  Nirajan Adhikari (10-11-2019)
+ *********************************************************************/
+#include "udf.h"
+#include "sg.h"
+/* Constants Definition */
+#define PRANDTL 0.67
+/* Thermal conductivity using constant Prandtl number */
+DEFINE_PROPERTY(THERMAL_CONDUCTIVITY_CONST_PRANDTL, c, t)
+{
+#if !RP_HOST /* Does nothing in Serial */
+	real Pr = PRANDTL;
+	real ktc;
+	real u_mu;
+	real u_cp;
+	u_cp = C_CP(c,t);
+	u_mu = C_MU_L(c,t); 
+	ktc = u_mu * u_cp / Pr;
+	return ktc;
+#endif
+}
